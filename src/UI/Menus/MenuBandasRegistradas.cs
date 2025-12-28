c4bca44e0c0407bc1ac43e0dcf711ceb4f16a307
Leonard Nucci de Oliveira <leonard.no@icloud.com>
2025-12-28 03:58:43 -0300

    feat: migra MenuBandasRegistradas para o padrão MenuComContexto

--- src/UI/Menus/MenuBandasRegistradas.cs
+++ src/UI/Menus/MenuBandasRegistradas.cs
@@ -2,20 +2,16 @@ using ScreenSound.Application;
 using ScreenSound.Utils;
 namespace ScreenSound.UI;
 
-internal class MenuBandasRegistradas : Menu<BandaService>
+internal class MenuBandasRegistradas : MenuComContexto<BandaService>
 {
-    private readonly SystemContext _systemContext;
-
-    public MenuBandasRegistradas(SystemContext contexto)
-    {
-        _systemContext = contexto;
-    }
-    protected override void ExibirConteudo(BandaService bandaService)
+    public MenuBandasRegistradas(SystemContext context, BandaService service) : base(context, service) {}
+    
+    protected override void ExibirConteudo()
     {
         int index = 1;
         ConsoleUtils.LimparTela();
         ExibirTituloDoMenu("Exibindo todas as bandas registradas na nossa aplicação");
-        foreach (var banda in _systemContext.ListaDeTodasAsBandas)
+        foreach (var banda in Context.ListaDeTodasAsBandas)
         {
             Console.WriteLine($"{index}. {banda.NomeDaBanda}");
             index++;
