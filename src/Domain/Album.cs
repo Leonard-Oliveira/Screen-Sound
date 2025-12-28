84174842521ca24378c844aaed053264e7417162
Leonard Nucci de Oliveira <leonard.no@icloud.com>
2025-12-28 01:15:28 -0300

    feat(domain): implementa IAvaliavel e padroniza encapsulamento na classe Album - Adiciona suporte à interface de avaliações e cálculo de média. - Protege as listas de músicas e avaliações com IReadOnlyCollection. - Aplica validações defensivas no construtor (Fail-Fast). - Organiza a estrutura interna do arquivo utilizando diretivas #region.

--- src/Domain/Album.cs
+++ src/Domain/Album.cs
@@ -1,28 +1,44 @@
+using System.Net;
+using Screensound.Domain.Interfaces;
+
 namespace ScreenSound.Domain;
-internal class Album
+internal class Album : IAvaliavel
 {   
+    #region Campos Privados (Backing Fields)
+    private readonly List<Avaliacao> _avaliacoes = new();
+    private readonly List<Musica> _musicasDoAlbum = new();
+    #endregion
+
+    #region Atributos e Propriedades
     public string NomeDoAlbum { get; private set; }
-
     public Banda BandaDoAlbum { get; private set; } 
-
     public int AnoDeLancamento { get; private set; }
 
-    public List<Musica> MusicasDoAlbum { get; private set; }
+    public IReadOnlyCollection<Avaliacao> Avaliacoes => _avaliacoes.AsReadOnly();
+    public IReadOnlyCollection<Musica> MusicasDoAlbum => _musicasDoAlbum.AsReadOnly();
 
+    public double AvaliacaoMedia => _avaliacoes.Any() ? _avaliacoes.Average(a => a.Nota) : 0.0;
+    #endregion
+
+    #region Construtor
     public Album(string albumName, Banda banda, int anoDeLancamento)
     {
         if (string.IsNullOrWhiteSpace(albumName)) throw new ArgumentException("O nome do álbum não pode ser vazio.");
-        if (banda == null) throw new ArgumentNullException("A banda do álbum não pode ser nula.");
-        if (anoDeLancamento <= 0) throw new ArgumentException("O ano de lançamento deve ser um número positivo.");
-        
         NomeDoAlbum = albumName;
-        BandaDoAlbum = banda;
+        BandaDoAlbum = banda ?? throw new ArgumentNullException(nameof(banda));
+        if (anoDeLancamento <= 0) throw new ArgumentException("O ano de lançamento deve ser um número positivo.");
         AnoDeLancamento = anoDeLancamento;
-        MusicasDoAlbum = new List<Musica>();
     }
+    #endregion
 
+    #region Métodos
     public void AdicionaMusicaAoAlbum(Musica musica)
     {
-        MusicasDoAlbum.Add(musica);
+        _musicasDoAlbum.Add(musica);
     }
+    public void AtribuiAvaliacao(Avaliacao avaliacao)
+    {
+        _avaliacoes.Add(avaliacao);
+    }
+    #endregion
 }
