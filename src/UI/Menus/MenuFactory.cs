2c8dbc12d4543e65fd5ef85cca8172d5c6a316d5
Leonard Nucci de Oliveira <leonard.no@icloud.com>
2025-12-28 03:59:53 -0300

    feat: implementa MenuFactory para centralizar a resolução de dependências

---
+++ src/UI/Menus/MenuFactory.cs
@@ -0,0 +1,55 @@
+using Microsoft.VisualBasic;
+using ScreenSound.Application;
+using ScreenSound.Domain;
+
+namespace ScreenSound.UI.Menus;
+
+internal class MenuFactory
+{
+    private readonly SystemContext _context;
+
+    public MenuFactory(SystemContext context)
+    {
+        _context = context;
+    }
+
+    // O Program.cs chamará estes métodos:
+    public MenuAvaliarAlbum CriarMenuAvaliarAlbum()
+    {
+        var service = new AlbumService(_context);
+        return new MenuAvaliarAlbum(_context, service);
+    }
+
+    public MenuRegistrarAlbum CriarMenuRegistrarAlbum()
+    {
+        var albumService = new AlbumService(_context);
+        var bandaService = new BandaService(_context);
+        return new MenuRegistrarAlbum(_context, albumService, bandaService);
+    }
+
+    public MenuDetalhesDaBanda CriarMenuDetalhesDaBanda()
+    {
+        // A fábrica resolve a dependência do serviço usando o contexto que ela já possui
+        var service = new BandaService(_context);
+        return new MenuDetalhesDaBanda(_context, service);
+    }
+
+    public MenuAvaliarBanda CriarMenuAvaliarBanda()
+    {
+        var service = new BandaService(_context);
+        return new MenuAvaliarBanda(_context, service);
+    }
+
+    public MenuRegistrarBanda CriarMenuRegistrarBanda()
+    {
+        var service = new BandaService(_context);
+        return new MenuRegistrarBanda(_context, service);
+    }
+
+    public MenuBandasRegistradas CriarMenuBandasRegistradas()
+    {
+        var service = new BandaService(_context);
+        return new MenuBandasRegistradas(_context, service);
+    }
+    // Conforme você criar novos menus, adicione-os aqui
+}
