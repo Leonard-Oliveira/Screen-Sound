08109ddfe48d07419481d98532804e5f10f53df0
Leonard Nucci de Oliveira <leonard.no@icloud.com>
2025-12-28 10:22:15 -0300

    style: organiza estrutura da classe EpisodioDePodcast seguindo padrões de Clean Code

--- src/Domain/EpisodioDePodcast.cs
+++ src/Domain/EpisodioDePodcast.cs
@@ -1,29 +1,39 @@
 namespace ScreenSound.Domain;
+
 internal class EpisodioDePodcast
-{   
-    // ATRIBUTOS
+{
+    #region Campos Privados (Backing Fields)
+    private readonly List<string> _convidados = new();
+    #endregion
+
+    #region Atributos e Propriedades
     public int NumeroDoEpisodio { get; private set; }
     public string TituloDoEpisodio { get; private set; }
     public int Duracao { get; private set; }
     public string ResumoDoEpisodio { get; private set; }
-    public List<string> ListaDeConvidados { get; private set; }
     public Podcast PodcastVinculado { get; private set; }
+    public IReadOnlyCollection<string> ListaDeConvidados => _convidados;
+    #endregion
 
-    // CONSTRUTOR
-    public EpisodioDePodcast(int numeroDoEpisodio, string tituloDoEpisodio, int duracao, string resumoDoEpisodio, Podcast podcastVinculado) 
+    #region Construtor
+    public EpisodioDePodcast(int numeroDoEpisodio, string tituloDoEpisodio, int duracao, string resumoDoEpisodio, Podcast podcastVinculado)
     {
         NumeroDoEpisodio = numeroDoEpisodio;
         TituloDoEpisodio = tituloDoEpisodio;
         Duracao = duracao;
         ResumoDoEpisodio = resumoDoEpisodio;
         PodcastVinculado = podcastVinculado;
-        ListaDeConvidados = new List<string>();
     }
+    #endregion
 
-    // MÉTODOS
+    #region Métodos
     public void AdicionarConvidaos(string nomeDoConvidado)
-    {   
-        ListaDeConvidados.Add(nomeDoConvidado);
-        Console.WriteLine($"Convidado {nomeDoConvidado} adicionado ao episódio {TituloDoEpisodio}.");
+    {
+        if (nomeDoConvidado != null)
+        {
+            _convidados.Add(nomeDoConvidado);
+            Console.WriteLine($"Convidado {nomeDoConvidado} adicionado ao episódio {TituloDoEpisodio}.");
+        }
     }
+    #endregion
 }
