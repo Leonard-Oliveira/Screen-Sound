62981b9426a492ec5c7058426101db1d5aacc808
Leonard Nucci de Oliveira <leonard.no@icloud.com>
2025-12-28 10:33:24 -0300

    feat: implementa entidade Avaliacao com validação de intervalo

--- src/Domain/Avaliacao.cs
+++ src/Domain/Avaliacao.cs
@@ -1,11 +1,15 @@
 namespace ScreenSound.Domain;
 internal class Avaliacao
 {
+    #region Atributos e Propriedades
     public int Nota { get; private set; }
+    #endregion
     
+    #region Métodos
     public Avaliacao(int nota)
     {
         if (nota < 1 || nota > 10) throw new ArgumentOutOfRangeException(nameof(nota), "A nota deve estar entre 1 e 10.");
         Nota = nota;
     }
+    #endregion
 }
